e5f8c33136c014ae3f6ec6bbcf27e6b0f7ea70fc
Camila18917 <camilagomez18917@gmail.com>
2024-08-07 11:09:09 -0300

    Escuela de artes marciales

---
+++ script.js
@@ -0,0 +1,9 @@
+function enviar(){
+    alert("Enviaste tu consulta con exito");
+}
+function ingresar1(){
+    alert("Bienvenido a tu area de entrenamiento");
+}
+function ingresar2(){
+    alert("Bienvenido a tu area de trabajo");
+}
